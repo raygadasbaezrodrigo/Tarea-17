3efcf70d0dece2d0f2f7e03f997ea31d7b4b1252
raygadasbaezrodrigo <31478555+raygadasbaezrodrigo@users.noreply.github.com>
2017-10-29 20:40:18 -0600

    Add files via upload

---
+++ Tarea 17.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main ()
+{
+	int var1=5,var2=2,resultado=0;
+	
+	//igual a ?
+	resultado=var1 == var2;
+	printf("%d == %d=%d\n",var1,var2,resultado);
+	
+	//menor igual que?	
+	resultado=var1 <= var2;
+	printf("%d <= %d=%d\n",var1,var2,resultado);
+	
+	//mayor que?
+	resultado=var1 > var2;
+	printf("%d > %d=%d\n",var1,var2,resultado);
+	
+	//diferente de ?
+	resultado=var1 != var2;
+	printf("%d != %d=%d\n",var1,var2,resultado);
+	
+	//uso de &&
+	
+	resultado=(var1 > var2) && (var1 != var2);
+	printf("(%d > %d) && (%d != %d)=%d\n",var1,var2,var1,var2,resultado);
+}
